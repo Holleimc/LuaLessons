0d8a194c83e1be80a527cb09fea07a64d410d82b
TKOFFICE\zhangshuo02 <zhangshuo02@mail.jj.com>
2021-12-09 21:49:39 +0800

    VSCode&Lua Debug Configure

---
+++ hellow.lua
@@ -0,0 +1,13 @@
+print("hellow lua")
+
+local function max(num1, num2)
+    if (num1 > num2) then
+        result = num1;
+    else
+        result = num2;
+    end
+    return result; 
+end
+ -- calling a function
+print("The maximum of the two numbers is ",max(10,4))
+print("The maximum of the two numbers is ",max(5,6))
